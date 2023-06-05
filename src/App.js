659efe0887b2a2748b0cd4ab69ac2981c7536628
Ksenija Kivojenko <kivojenko@gmail.com>
2023-06-05 23:35:52 +0300

    Music added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
+import music from './music.mp3'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return <>
+        <div className="musicOn" style={{ marginLeft: "30%"}}>
+            <h1>Bzz Project</h1>
+            <audio id="audio" controls loop>
+                <source src={music}/>
+            </audio>
+        </div>
+    </>
+;
 }
 
 export default App;
