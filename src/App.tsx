27314d5fc383902c4f8405afc97534792e13d949
Ksenija Kivojenko <ksenija.kivojenko@gamesys.ee>
2023-07-06 15:49:26 +0300

    Add saved theme by default

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import {Layout} from "./components/Layout";
 import {Home} from "./pages/Home";
+import { getTheme } from "./service/themeService";
 
 function App() {
+    getTheme();
     return (
         <Routes>
             <Route path="/" element={<Layout/>}>
