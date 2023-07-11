880225bfeca2c0977341acb233f63978359ec28b
Ksenija Kivojenko <ksenija.kivojenko@gamesys.ee>
2023-07-11 17:04:13 +0300

    Code clean up

--- src/components/basicComponents/Bee.tsx
+++ src/components/basicComponents/Bee.tsx
@@ -3,7 +3,8 @@ import { ReactComponent as BeeSVG } from "../../assets/images/bee.svg";
 
 export const Bee = () => {
   return (
-    <div id="bee">
-      <BeeSVG width="100px" height="100px"/>
-    </div>)
+    <div id="bee-container">
+      <BeeSVG id="bee" width="100px" height="100px"/>
+    </div>
+  )
 }
