519938c2985e34390dfb8ebabe232df49bd533a6
Ksenija Kivojenko <ksenija.kivojenko@gamesys.ee>
2023-07-18 17:08:36 +0300

    Migrate bee behaviour from css to script

--- src/assets/scripts/bee.ts
+++ src/assets/scripts/bee.ts
@@ -1,5 +1,22 @@
 import { useState } from "react";
 
+const anglesMatrix = [
+  [[180, 270], [90, 270], [90, 180]],
+  [[180, 360], [0, 360], [0, 180]],
+  [[270, 360], [-90, 90], [0, 90]]
+]
+
+const anglesSides = {
+  north_east: [0, 90],
+  north: [0, 180],
+  north_west: [90, 180],
+  west: [90, 270],
+  south_west: [180, 270],
+  south: [180, 360],
+  south_east: [270, 360],
+  east: [-90, 90]
+}
+
 function getAngleInRadians(angle: number) {
   return (angle * Math.PI) / 180;
 }
@@ -11,6 +28,26 @@ function getBottomDistance(angle: number, distance: number) {
 
 function getLeftDistance(angle: number, distance: number) {
   return distance * Math.cos(getAngleInRadians(angle));
+}
+
+function getNewAngle(leftChange: number, bottomChange: number) {
+  let [minimum, maximum] = [0, 360]
+  switch (bottomChange) {
+    case 1:
+      switch (leftChange) {
+        case -1:
+
+          break;
+        case 1:
+          break;
+      }
+      break;
+    case -1:
+      maximum = 360;
+      break;
+  }
+  const newAngle = Math.random() * (maximum - minimum) + minimum;
+  return newAngle % 360;
 }
 
 export const useBeePosition = () => {
@@ -33,9 +70,9 @@ export const useBeePosition = () => {
     let bottomChange = 0;
     let leftChange = 0;
     if (left + leftToMove < 100) {
-      leftChange = 1;
-    } else if (left + leftToMove > window.innerWidth - 100) {
       leftChange = -1;
+    } else if (left + leftToMove > window.innerWidth - 100) {
+      leftChange = 1;
     }
     if (bottom + bottomToMove < 100) {
       bottomChange = -1;
@@ -43,33 +80,10 @@ export const useBeePosition = () => {
       bottomChange = 1;
     }
     if (leftChange != 0 || bottomChange != 0 || Math.random() > 0.9) {
-      let [minimum, maximum] = [0, 360]
-      if (leftChange > 0) {
-        if (bottomChange > 0) {
-          minimum = 270;
-        } else if (bottomChange < 0) {
-          maximum = 90;
-        } else {
-          minimum = -90;
-          maximum = 90;
-        }
-      } else if (leftChange < 0) {
-        if (bottomChange > 0) {
-          minimum = 180;
-          maximum = 270;
-        } else if (bottomChange < 0) {
-          minimum = 90;
-          maximum = 180;
-        } else {
-          minimum = 90;
-          maximum = 270;
-        }
-      }
-      const newAngle = Math.random() * (maximum - minimum) + minimum;
-      setAngle(newAngle % 360);
+      setAngle(getNewAngle(leftChange, bottomChange));
     }
     moveBee(angle, distance)
   }, 1000);
 
-  return [left, bottom, angle]
+  return [left, bottom, 0, 0, angle]
 }
--- src/components/basicComponents/Bee.tsx
+++ src/components/basicComponents/Bee.tsx
@@ -5,14 +5,17 @@ import { useBeePosition } from "../../assets/scripts/bee";
 
 
 export const Bee = () => {
-  const [x, y, angle] = useBeePosition();
+  const [x, y, rotateX, rotateY, angle] = useBeePosition();
 
   return (
     <div id="bee-container">
       <BeeSVG id="bee" width="100px" height="100px" style={{
         left: x + 'px',
         bottom: y + 'px',
-        transform: "rotateY(" + ((270 < angle || angle < 90) ? 180 : 0) + "deg) rotate(" + angle % 180 + "deg)"
+        transform:
+          "rotateX(" + 0 + "deg) " +
+          "rotateY(" + ((270 < angle || angle < 90) ? 180 : 0) + "deg) " +
+          "rotate(" + angle % 180 + "deg)"
       }}/>
     </div>);
 }
